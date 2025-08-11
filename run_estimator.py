e6e97874613db96272ea880611a4e97bccdd1ead
Marina Centenera Merino <126160075+Mcentenera@users.noreply.github.com>
2025-08-11 11:39:42 +0200

    Adding files for version v.1.7

--- run_estimator.py
+++ run_estimator.py
@@ -7,27 +7,47 @@ from src.utils import snr_cal
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument("params", nargs=10)
+parser.add_argument("params", nargs=14)
 args = parser.parse_args()
 
 
 typed = [
-    float(args.params[0]), float(args.params[1]), float(args.params[2]), args.params[3] == "False",
-    float(args.params[4]), int(args.params[5]), int(args.params[6]), float(args.params[7]),
-    args.params[8] == "True", args.params[9] == "False"
+    float(args.params[0]), float(args.params[1]), float(args.params[2]), args.params[3] == "False", float(args.params[4]),
+    int(args.params[5]), int(args.params[6]), float(args.params[7]), float(args.params[8]), float(args.params[9]), float(args.params[10]), float(args.params[11]) args.params[12] == "True", args.params[13] == "False"
     ]
 
-results, module_diameter_m, com_core_out_mm = marcot_hr_estimator(*typed)
-print_results(results, module_diameter_m, com_core_out_mm)
+
+results_list = []
+
+results = marcot_hr_estimator(*typed)
+print_results(results)
 
 filter_data = {k: v for k, v in results.items() if isinstance(v, (int, float, np.ndarray)) and k != ""}
 
-df = pd.DataFrame.from_dict(filter_data, orient = 'index', columns=["# Value"])
-df.to_csv("data/results_marcot.txt", sep = '\t', index = False)
-print("\033[1;4;32mFile 'results_marcot.txt' was saved successfully\033[0m")
+results_list.append(filter_data)
 
-MCM = multi_criteria('data/results_marcot.txt')
+df_results = pd.DataFrame(results_list)
+
+# df = pd.DataFrame.from_dict(filter_data, orient = 'index', columns=["# Value"])
+
+df_results.to_csv("data/results_marcot.csv", sep = '\t', index = False)
+print("\033[1;4;32mFile 'results_marcot.csv' was saved successfully\033[0m")
+
+def print_section(title):
+    print(f"\n{'=' * 60}\n{title:^60}\n{'=' * 60}")
+
+print_section("SCIENTIFIC CRITERIA")
+fraction = snr_cal('data/results_marcot.csv')
+    # HAY QUE DEFINIR UN VALOR MÁS REALISTA DE LA EFICIENCIA DEL SISTEMA DE MARCOT Y NO PONER UN 0.9 POR DEFECTO
+    
+    # HAY QUE CAMBIAR EL module_diameter_m, POR PARÁMETROS n_otas Y diameter_ota_m PARA QUE CALCULA LA APERTURA EFECTIVA
+    
+print(f"MARCOT telescope has a {np.round(fraction, 3)} * SNR in comparison to a traditional design")
+
+MCM = multi_criteria('data/results_marcot.csv')
 print(MCM)
+
+
 
 
 
